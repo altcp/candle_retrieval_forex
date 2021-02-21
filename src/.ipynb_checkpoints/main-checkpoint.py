4ec1b183ee9782b4b3c3e60db456edf33f667cef
altcp <alvin.tan@ttrtea.com>
2021-02-22 00:22:02 +0800

    Preparing for Release

---
+++ src/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import functions as f
+import datetime
+import gc
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#Tradable Symbols
+def get_symbols(sym):
+    
+    switcher = {
+        
+        #List of Desired Instruments
+        0: 'EUR_CHF',
+        1: 'EUR_GBP',
+        2: 'EUR_USD',
+        3: 'USD_CHF',
+        4: 'GBP_USD',
+        5: 'EUR_AUD',
+        6: 'AUD_CHF',
+        7: 'CHF_JPY',
+        8: 'EUR_JPY',
+        9: 'GBP_JPY',
+        
+        10: 'USD_CAD',
+        11: 'AUD_USD',
+        12: 'NZD_USD',
+        13: 'USD_JPY',
+        14: 'USD_SGD',
+        15: 'AUD_CAD',
+        16: 'CAD_JPY',
+        17: 'AUD_NZD',
+        18: 'AUD_JPY',
+        19: 'NZD_JPY',
+        
+        20: 'GBP_AUD',
+        21: 'EUR_NZD',
+        22: 'GBP_CAD',
+        23: 'CAD_CHF',
+        24: 'GBP_NZD',
+        25: 'EUR_CAD',
+        26: 'GBP_CHF',
+        27: 'AUD_SGD',
+        28: 'NZD_CHF',
+        29: 'SGD_JPY',
+        30: 'NZD_CAD',
+        
+    }
+    
+    return switcher.get(sym, 'ERR')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+def main():
+    
+    ts = round(datetime.datetime.now().timestamp(), 0)
+    print(" ")
+    print("OFDC: Activated at " +  str(ts))
+    print(" ")
+    
+    #Update all Tradeable Symbols
+    for i in range(31):
+        
+        try:
+            sym = get_symbols(i) 
+            df1 = f.get_data(sym)
+            f.update_db(df1, sym)
+        except:
+            print("ERR. Get and Update - " + sym)
+            continue
+    
+    #Post DB
+    f.post_db() 
+    ts2 = round(datetime.datetime.now().timestamp(), 0)
+    tim = round(((ts2 - ts)/60), 2)    
+    msg = f"OFDC: Done, Deactivated in {tim} Minutes." 
+    print(" ")
+    print(msg)
+    
+    return None
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    main()
+    gc.collect()    
+    quit()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
--- src/functions.py
+++ src/functions.py
@@ -1,2 +1,239 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
 import json
+import yaml
+import boto3
+import ntplib
+import sqlite3
+import requests
+import datetime
+import numpy as np
+import pandas as pd
+
+from dateutil import parser
+from botocore.client import Config
+from datetime import date, timedelta
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+def get_data(sym):
+    
+    #Configurations
+    with open('./config/config.yaml') as file:
+        config = yaml.safe_load(file)
+        
+    authorization = config['configuration']['authorization'] 
+    url = config['configuration']['oanda']
+    granuality = config['configuration']['granuality']
+    data_start = config['configuration']['data_start']
+    data_end = config['configuration']['data_end']
+    
+    
+    #Get Time
+    x = ntplib.NTPClient()
+    
+    try:
+        now = datetime.datetime.utcfromtimestamp(x.request('uk.pool.ntp.org').tx_time)
+    except:
+        try:
+            now = datetime.datetime.utcfromtimestamp(x.request('europe.pool.ntp.org').tx_time)
+        except:
+            now = datetime.datetime.now()
+            
+            
+    def check_weekday(date):
+        # Mon-Fri (0-4)
+        while date.weekday() > 4: 
+            date -= timedelta(days=1)
+        return date
+    
+    
+    #Time to Epoch
+    start = now.replace(hour=data_start, minute=0, second=0, microsecond=0)
+    end = now.replace(hour=data_end, minute=0, second=0, microsecond=0)
+    weekday_start = check_weekday(start)
+    weekday_end = check_weekday(end)
+    start_epoch = weekday_start.timestamp()
+    end_epoch = weekday_end.timestamp()
+    
+
+    #Headers and Parameters
+    payload_1 = {'from': start_epoch, 'to': end_epoch, 'price': 'M', 'granularity': granuality}
+    auth = {"Authorization": "Bearer " + authorization}
+    url = url + sym + "/candles" 
+    
+    
+    #Request 1
+    response = requests.get(url, params=payload_1, headers=auth)     
+    raw = response.json()
+    data = json.dumps(raw, indent=2)
+    dataset = pd.read_json(data)
+    df = pd.json_normalize(dataset['candles'])
+    del df['complete']
+
+    df['time'] = pd.to_datetime(df['time']).dt.tz_localize(None)
+    df.columns = ['vol','time', 'open','high','low', 'close']
+    df1 = df[['time','vol','high','low','open', 'close']]
+    
+    return df1
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+def update_db(df_one, symbol):
+    
+    #Configurations
+    with open('./config/config.yaml') as file:
+        config = yaml.safe_load(file)
+        
+    db_path = config['locations']['db_path']
+    granuality = config['configuration']['granuality']
+    
+    table_one_name = symbol + "_" + granuality
+    conn = sqlite3.connect(db_path)
+    
+    try:
+        
+        #Check if Data Exists
+        query = "SELECT * FROM [" + table_one_name + "]"
+        temp_date = pd.read_sql_query(query, conn)
+    
+        last_r_time = parser.parse(temp_date.at[(len(temp_date.index) - 1), 'time'])
+        current_r_time = parser.parse(str(df_one.at[(len(df_one.index) - 1), 'time']))
+    
+        if(last_r_time >= current_r_time):
+            print("Record Exists in Local DB - " + symbol)   
+        else:
+            #Update
+            df_one.to_sql(name=table_one_name, con=conn, if_exists='append')
+            print("Local DB Updated - " + symbol)
+        
+    except:
+        #Insert
+        df_one.to_sql(name=table_one_name, con=conn, if_exists='replace')
+        print("Local DB inserted - " + symbol)
+    
+    conn.close()
+        
+    return None
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+def post_db():
+
+    #Configurations
+    with open('./config/config.yaml') as file:
+        config = yaml.safe_load(file)
+        
+    minio_key = config['settings']['minio_key']
+    minio_access = config['settings']['minio_access']
+    
+    db_path = config['locations']['db_path']
+    server_path = config['locations']['server_path']
+    server_bucket = config['locations']['server_bucket']
+
+    
+    #Get Time
+    x = ntplib.NTPClient()
+    
+    try:
+        now = datetime.datetime.utcfromtimestamp(x.request('uk.pool.ntp.org').tx_time)
+    except:
+        try:
+            now = datetime.datetime.utcfromtimestamp(x.request('europe.pool.ntp.org').tx_time)
+        except:
+            now = datetime.datetime.now()
+    
+
+    #Upload
+    try:
+        s3 = boto3.resource('s3', endpoint_url=server_path, aws_access_key_id=minio_key, 
+                            aws_secret_access_key=minio_access,
+                            config=Config(signature_version='s3v4'), 
+                            region_name='us-east-1' )
+        
+        posting_date = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        file = str(posting_date.weekday()) + "-OANDA.db"
+        
+        s3.Bucket(server_bucket).upload_file(db_path, file)       
+        print("Upload Successful.")
+     
+    except:
+        print("Upload Failed.")
+
+    return None
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
